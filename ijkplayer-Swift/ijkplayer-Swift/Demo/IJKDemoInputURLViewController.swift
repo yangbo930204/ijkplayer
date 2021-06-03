964ff7e81b0fdba51cb0ac1dd9e00090fdb96149
yangbo930204 <200956721@qq.com>
2021-06-03 14:06:20 +0800

    commit message

---
+++ ijkplayer-Swift/ijkplayer-Swift/Demo/IJKDemoInputURLViewController.swift
@@ -0,0 +1,44 @@
+//
+//  IJKDemoInputURLViewController.swift
+//  ijkplayer-Swift
+//
+//  Created by iOS Dev Log on 2017/8/24.
+//  Copyright © 2017年 iOSDevLog. All rights reserved.
+//
+
+import UIKit
+
+class IJKDemoInputURLViewController: UIViewController, UITextViewDelegate {
+    let rtmp = "rtmp://live.hkstv.hk.lxdns.com/live/hks"
+    let m3u8 = "https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8"
+    let rtsp = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov"
+    
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Input URL"
+        textView.delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Play", style: .done, target: self, action: #selector(self.onClickPlayButton))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        textView.text = rtsp
+    }
+    
+    @objc func onClickPlayButton() {
+        let url = URL(string: textView.text)
+        let scheme: String? = url?.scheme?.lowercased()
+        if (scheme == "http") || (scheme == "https") || (scheme == "rtmp") || (scheme == "rtsp") {
+            IJKPlayerViewController.present(from: self, withTitle: "URL: \(String(describing: url))", url: url!, completion: {() -> Void in
+                self.navigationController?.popViewController(animated: false)
+            })
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        onClickPlayButton()
+    }
+}
---
+++ ijkplayer-Swift/ijkplayer-Swift/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  ijkplayer-Swift
+//
+//  Created by iOS Dev Log on 2017/8/24.
+//  Copyright © 2017年 iOSDevLog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var player:IJKFFMoviePlayerController!
+    
+    let urls = ["https://transpox.ai/vod/VIdDITnI80yakUBjzgr6LOu5Y3fWcBnHssA3bkcf_P0/27-8-2018_7h38m11s.mp4/index.m3u8",
+                "rtmp://v1.one-tv.com:1935/live/mpegts.stream"]
+    
+    @IBOutlet weak var videoView: UIView!
+    fileprivate func play(_ url: URL?) {
+        let options = IJKFFOptions.byDefault()
+        
+        
+        //初始化播放器，播放在线视频或直播（RTMP）
+        let player = IJKFFMoviePlayerController(contentURL: url!, with: options)
+        //播放页面视图宽高自适应
+        player?.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        player?.view.frame = self.videoView.bounds
+        player?.scalingMode = .aspectFit //缩放模式
+        player?.shouldAutoplay = true //开启自动播放
+        
+        self.videoView.autoresizesSubviews = true
+        self.videoView.addSubview((player?.view)!)
+        self.player = player
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //视频源地址
+        let url = URL(string: urls[1])
+        
+        play(url)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //开始播放
+        self.player.prepareToPlay()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        //关闭播放器
+        self.player.shutdown()
+    }
+    
+    @IBAction func playRTMP(_ sender: UIButton) {
+        self.player.shutdown()
+        
+        let url = URL(string: urls[0])
+        play(url)
+        self.player.prepareToPlay()
+    }
+    
+    @IBAction func playM3u8(_ sender: UIButton) {
+        self.player.shutdown()
+        
+        let url = URL(string: urls[1])
+        play(url)
+        self.player.prepareToPlay()
+    }
+}
+
